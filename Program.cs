de0c0c90424f5e1386b74ea45c31ac350cce0996
Vinicius Dos Santos Grana <54106581833@info.local>
2023-03-29 10:57:17 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Write($"Digite as notas1");
+float nota1 = float.Parse(Console.ReadLine());
+Console.Write($"Digite as notas2");
+float nota2 = float.Parse(Console.ReadLine());
+Console.Write($"Digite as notas3");
+float nota3 = float.Parse(Console.ReadLine());
+Console.Write($"Digite as notas4");
+float nota4 = float.Parse(Console.ReadLine());
+Console.Write($"Digite as notas5");
+float nota5 = float.Parse(Console.ReadLine());
+
+
+
+
+
+float media = ((nota1 + nota2 + nota3 + nota4 + nota5) /5);
+
+
+
+Console.WriteLine($"Sua média é de :{media}");
